d1064062db2be55f08fd86d587af7a804f26dea2
Swarnavo2003 <swarnavo2020@gmail.com>
2025-10-16 20:47:25 +0530

    first commit

---
+++ src/main/java/com/projects/ecommerce/service/ProductService.java
@@ -0,0 +1,23 @@
+package com.projects.ecommerce.service;
+
+import com.projects.ecommerce.model.Product;
+import com.projects.ecommerce.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo repo;
+
+    public List<Product> getAllProducts() {
+        return repo.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return repo.findById(id).orElse(null);
+    }
+}
